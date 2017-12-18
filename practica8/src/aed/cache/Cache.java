6c20cc397395e1c4c9db8444f98038426ffa5d56
Máximo J. García Martínez <onmax@users.noreply.github.com>
2017-12-18 11:49:05 +0100

    test 14

--- practica8/src/aed/cache/Cache.java
+++ practica8/src/aed/cache/Cache.java
@@ -6,45 +6,36 @@ import es.upm.aedlib.Position;
 import es.upm.aedlib.map.*;
 import es.upm.aedlib.positionlist.*;
 
-
-public class Cache<Key,Value> {
+public class Cache<Key, Value> {
 	private int maxCacheSize;
-	private Storage<Key,Value> storage;
-	private Map<Key,CacheCell<Key,Value>> map;
+	private Storage<Key, Value> storage;
+	private Map<Key, CacheCell<Key, Value>> map;
 	private PositionList<Key> lru;
 
-	public Cache(int maxCacheSize, Storage<Key,Value> storage) {
+	public Cache(int maxCacheSize, Storage<Key, Value> storage) {
 		this.storage = storage;
-		this.map = new HashTableMap<Key,CacheCell<Key,Value>>();
+		this.map = new HashTableMap<Key, CacheCell<Key, Value>>();
 		this.lru = new NodePositionList<Key>();
 		this.maxCacheSize = maxCacheSize;
 	}
 
 	public Value get(Key key) {
 		Value res = null;
-		boolean esta=false;
-		Iterator<Key> cursor=map.keys();
-		while(cursor.hasNext()) {
-			if(cursor.next().equals(key)) {
-				esta=true;
-			}
-		}
-
-		if(esta) {
-			Position<Key> aux=lru.first();
-			lru.addBefore(lru.first(),key);
-			lru.remove(aux);
-			res = storage.read(key);
-		}
-		else {
+		if (map.get(key) != null) {
+			lru.addFirst(key);
+			lru.remove(map.get(key).getPos());
+			res = map.get(key).getValue();
+			map.get(key).setValue(res);
+			map.get(key).setPos(lru.first());
+		} else {
 			res = storage.read(key);
 			lru.addFirst(key);
-			CacheCell cell = new CacheCell<Key, Value>(res,false,lru.first());
+			CacheCell cell = new CacheCell<Key, Value>(res, false, lru.first());
 			map.put(key, cell);
-			if(lru.size()>this.maxCacheSize) {
+			if (lru.size() > this.maxCacheSize) {
 				map.remove(lru.last().element());
 				lru.remove(lru.last());
-				if(cell.getDirty()) {
+				if (cell.getDirty()) {
 					storage.write(key, res);
 				}
 			}
@@ -54,35 +45,26 @@ public class Cache<Key,Value> {
 
 	public void put(Key key, Value value) {
 		Value res = null;
-		boolean esta=false;
-		Iterator<Key> cursor=map.keys();
-		while(cursor.hasNext()) {
-			if(cursor.next().equals(key)) {
-				esta=true;
-			}
-		}
-
-		if(esta) {
-			Position<Key> aux=lru.first();
-			lru.addBefore(lru.first(),key);
-			lru.remove(aux);
-			res = storage.read(key);
-		}
-		else {
+		if (map.get(key) != null) {
+			lru.addFirst(key);
+			lru.remove(map.get(key).getPos());
+			res = map.get(key).getValue();
+			map.get(key).setValue(res);
+			map.get(key).setPos(lru.first());
+		} else {
 			res = storage.read(key);
 			lru.addFirst(key);
-			CacheCell cell = new CacheCell<Key, Value>(res,false,lru.first());
+			CacheCell cell = new CacheCell<Key, Value>(res, false, lru.first());
 			map.put(key, cell);
-			if(lru.size()>this.maxCacheSize) {
-				map.remove(lru.first().element());
+			if (lru.size() > this.maxCacheSize) {
+				map.remove(lru.last().element());
 				lru.remove(lru.last());
-				if(cell.getDirty()) {
+				if (cell.getDirty()) {
 					storage.write(key, res);
 				}
 			}
-			cell.setValue(value);
-			cell.setDirty(true);
-
 		}
+		map.get(key).setValue(value);
+		map.get(key).setDirty(true);
 	}
 }
